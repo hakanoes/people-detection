313fe00056ff94ed89fe872197063b2c48eb39ec
Hakan Ören <64734720+hakanoes@users.noreply.github.com>
2020-05-24 20:12:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,8 +3,6 @@ import numpy as np
 
 cap = cv2.VideoCapture("video.mp4")
 people = cv2.CascadeClassifier("/home/hakanoes/people/haarcascade_fullbody.xml")
-fgbg = cv2.createBackgroundSubtractorMOG2()
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -12,16 +10,14 @@ while cap.isOpened():
     resize = frame[200:800, 400:1500]
     gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    fgmask = fgbg.apply(blur)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    body = people.detectMultiScale(fgmask, 1.3,  5)
+    body = people.detectMultiScale(blur, 1.3,  5)
 
 
     for (x, y, w, h) in body:
         cv2.rectangle(resize, (x, y), ((x + y), (w + h)), (0, 255, 0), 2)
         
     
-    cv2.imshow("resize",fgmask)
+    cv2.imshow("resize",resize)
     key = cv2.waitKey(30)
     if key == ord('q'):
         break
